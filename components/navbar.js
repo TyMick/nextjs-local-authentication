bad10fd8d88410374f592b8bf5edf94556f79bc5
Ty Mick <ty@tymick.me>
2019-11-19 14:38:33 -0600

    Create Navbar component

---
+++ components/navbar.js
@@ -0,0 +1,10 @@
+"use strict";
+
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+
+export default props => (
+  <Navbar bg="light" className="justify-content-center">
+    <Navbar.Brand>&#x2728; Stars Align &#x2728;</Navbar.Brand>
+  </Navbar>
+);
