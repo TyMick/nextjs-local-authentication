c406778e1fb0537e4664a7e900129ab8ba1d551c
Ty Mick <ty@tymick.me>
2019-11-19 14:29:07 -0600

    Create TabNav component

---
+++ components/tab-nav.js
@@ -0,0 +1,17 @@
+"use strict";
+
+import React from "react";
+import Link from "next/link";
+import Nav from "react-bootstrap/Nav";
+
+const links = ["Dashboard", "Events", "Friends", "Groups", "Settings"];
+
+export default (props) => (
+  <Nav variant="tabs" activeKey={"/" + props.active}>
+    {links.map(link => (
+      <Link href={"/" + link.toLowerCase()} passHref key={link.toLowerCase()}>
+        <Nav.Link>{link}</Nav.Link>
+      </Link>
+    ))}
+  </Nav>
+);
