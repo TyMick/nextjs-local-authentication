89804b416f5f59dbed6d64d9aa8c1245ea291d0f
Ty Mick <ty@tymick.me>
2019-11-19 12:02:21 -0600

    Separate create-next-app example pages

--- pages/index-example.js
+++ pages/index-example.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import Nav from '../components/nav-example'
 import "../styles.scss";
 
 const Home = () => (
