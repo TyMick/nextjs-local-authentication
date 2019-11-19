701d7b58e97ba1a1fa84a75313f3dedcc5ee6bb3
Ty Mick <ty@tymick.me>
2019-11-19 15:44:09 -0600

    Populate basic pages

---
+++ pages/settings.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import React from "react";
+import PageLayout from "../components/page-layout";
+import "../styles.scss";
+
+export default () => (
+  <PageLayout title="Dashboard">
+    
+  </PageLayout>
+);
