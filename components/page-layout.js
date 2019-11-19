6aa867d622ff3ca09088d24620644c2b9aaf3ee0
Ty Mick <ty@tymick.me>
2019-11-19 15:35:11 -0600

    Create PageLayout component

---
+++ components/page-layout.js
@@ -0,0 +1,26 @@
+"use strict";
+
+import Head from "next/head";
+import Navbar from "./navbar";
+import Container from "react-bootstrap/Container";
+import TabNav from "./tab-nav";
+
+export default ({ children, title }) => (
+  <div>
+    <Head>
+      <title>{"Stars Align" + (title && " | " + title)}</title>
+    </Head>
+
+    <header>
+      <Navbar />
+    </header>
+
+    <main>
+      <Container className="pt-3">
+        <TabNav active={title.toLowerCase()} />
+
+        {children}
+      </Container>
+    </main>
+  </div>
+);
