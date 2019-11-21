0de07b8a22cfc566bccef24fd92f66447c72677f
Ty Mick <ty@tymick.me>
2019-11-21 09:57:40 -0600

    Add login/logout links to navbar

--- components/navbar.js
+++ components/navbar.js
@@ -2,9 +2,14 @@
 
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default props => (
-  <Navbar bg="dark" variant="dark" className="justify-content-center">
+  <Navbar bg="dark" variant="dark" className="justify-content-between">
     <Navbar.Brand>&#x2728; Stars Align &#x2728;</Navbar.Brand>
+    <Nav>
+      <Nav.Link href="#">Login</Nav.Link>
+      <Nav.Link href="#">Logout</Nav.Link>
+    </Nav>
   </Navbar>
 );
