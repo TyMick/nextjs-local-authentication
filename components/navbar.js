db13bfff8a55941ad493f656414d5b1cdcfc2aea
Ty Mick <ty@tymick.me>
2019-11-22 08:13:08 -0600

    Add login page

--- components/navbar.js
+++ components/navbar.js
@@ -3,12 +3,13 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Link from "next/link";
 
 export default props => (
   <Navbar bg="dark" variant="dark" className="justify-content-between">
     <Navbar.Brand>&#x2728; Stars Align &#x2728;</Navbar.Brand>
     <Nav>
-      <Nav.Link href="#">Login</Nav.Link>
+      <Link href="/login" passHref><Nav.Link>Login</Nav.Link></Link>
       <Nav.Link href="#">Logout</Nav.Link>
     </Nav>
   </Navbar>
---
+++ pages/login.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import React from "react";
+import PageLayout from "../components/page-layout";
+import "../styles.scss";
+import Button from "react-bootstrap/Button";
+
+export default () => (  
+  <PageLayout title="Login">
+    <Button>Log in with Facebook</Button>
+  </PageLayout>
+);
