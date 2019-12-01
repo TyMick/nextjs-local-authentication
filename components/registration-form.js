a98e1bd15c9ea85ca4168c48591117c5f5b47b2b
Ty Mick <ty@tymick.me>
2019-12-01 15:45:09 -0600

    Add Formik logic to registration form

--- components/registration-form.js
+++ components/registration-form.js
@@ -1,53 +1,73 @@
 "use strict";
 
-import React, { useState } from "react";
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 import Form from "react-bootstrap/Form";
 import InputGroup from "react-bootstrap/InputGroup";
 import Button from "react-bootstrap/Button";
 
 export default () => {
-  const [validated, setValidated] = useState(false);
+  return (
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={Yup.object({
+        username: Yup.string()
+          .matches(/^\w+$/, "Username can only consist of letters, numbers, and underscores.")
+          .required("Please choose a username."),
+        password: Yup.string()
+          .required("Please choose a password.")
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {formik => (
+        <Form noValidate onSubmit={formik.handleSubmit} className="mx-auto" style={{ width: "100%", maxWidth: "420px" }}>
+          <Form.Group controlId="username">
+            <Form.Label>Username</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="ampersand">@</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                name="username"
+                aria-describedby="ampersand"
+                type="text"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.username}
+                isInvalid={formik.touched.username && !!formik.errors.username}
+              />
+              <Form.Control.Feedback type="invalid">
+                {formik.errors.username}
+              </Form.Control.Feedback>
+            </InputGroup>
+          </Form.Group>
 
-  const handleSubmit = event => {
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+          <Form.Group controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              isInvalid={formik.touched.password && !!formik.errors.password}
+            />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.password}
+            </Form.Control.Feedback>
+          </Form.Group>
 
-    setValidated(true);
-  }
-
-  return (  
-    <Form noValidate validated={validated} onSubmit={handleSubmit} className="mx-auto" style={{ width: "100%", maxWidth: "420px" }}>
-      <Form.Group controlId="username">
-        <Form.Label>Username</Form.Label>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="at">@</InputGroup.Text>
-          </InputGroup.Prepend>
-          <Form.Control
-            type="text"
-            aria-describedby="inputGroupPrepend"
-            required
-          />
-          <Form.Control.Feedback type="invalid">
-            Please choose a username.
-          </Form.Control.Feedback>
-        </InputGroup>
-      </Form.Group>
-
-      <Form.Group controlId="password">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" required />
-        <Form.Control.Feedback type="invalid">
-          Please choose a password.
-        </Form.Control.Feedback>
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  )
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      )}
+    </Formik>
+  );
 };
