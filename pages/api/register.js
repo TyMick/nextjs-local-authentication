b016c836a98276dd59e0db3a9b07046b91c56ef5
Ty Mick <ty@tymick.me>
2019-12-02 09:28:53 -0600

    Adjust registration error status code

--- pages/api/register.js
+++ pages/api/register.js
@@ -32,7 +32,7 @@ export default async (req, res) => {
     const { response } = err;
     response
       ? res.status(response.status).json({ message: response.statusText })
-      : res.status(400).json({ message: err.message });
+      : res.status(500).json({ message: err.message });
   }
 
   client.close();
