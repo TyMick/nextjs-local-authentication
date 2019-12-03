e98f850be0395f8aaef9baae8237d7ce30751f4b
Ty Mick <ty@tymick.me>
2019-12-03 09:34:03 -0600

    Add auth sync to require pages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,9 +3,12 @@
 import React from "react";
 import PageLayout from "../components/page-layout";
 import "../styles.scss";
+import { withAuthSync } from "../utils/auth";
 
-export default () => (
+const Dashboard = () => (
   <PageLayout title="Dashboard">
     
   </PageLayout>
 );
+
+export default withAuthSync(Dashboard);
--- pages/settings.js
+++ pages/settings.js
@@ -3,9 +3,12 @@
 import React from "react";
 import PageLayout from "../components/page-layout";
 import "../styles.scss";
+import { withAuthSync } from "../utils/auth";
 
-export default () => (
+const Settings = () => (
   <PageLayout title="Settings">
     
   </PageLayout>
 );
+
+export default withAuthSync(Settings);
