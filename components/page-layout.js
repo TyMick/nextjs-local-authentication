88259be2ff8fb0c8df633fb1ac4d9405af9171f2
Ty Mick <ty@tymick.me>
2019-12-15 18:55:32 -0600

    Add favicon

--- components/page-layout.js
+++ components/page-layout.js
@@ -9,6 +9,10 @@ export default ({ children, title }) => (
   <div className="bg-light">
     <Head>
       <title>{"Stars Align" + (title && " | " + title)}</title>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
     </Head>
 
     <header>
