4f7173d3d9280a7ab80c5f59aba6e6a16dc13392
Ty Mick <ty@tymick.me>
2019-12-29 09:31:07 -0800

    Fix CSS (adds a lot of unnecessary files to remove later)

--- next.config.js
+++ next.config.js
@@ -6,5 +6,14 @@ const withSass = require("@zeit/next-sass");
 module.exports = withSass({
   env: {
     DB: process.env.DB
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        "file-loader",
+      ],
+    })
+    return config;
   }
 });
