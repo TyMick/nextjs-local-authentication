b2b4bf5251ea39e9e001a59a87365dbc11b54df8
Ty Mick <ty@tymick.me>
2019-12-29 22:59:54 -0600

    Fix icon font loading

--- next.config.js
+++ next.config.js
@@ -2,18 +2,10 @@
 
 require("dotenv").config();
 const withSass = require("@zeit/next-sass");
+const withFonts = require("next-fonts");
 
-module.exports = withSass({
+module.exports = withSass(withFonts({
   env: {
     DB: process.env.DB
-  },
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/,
-      use: [
-        "file-loader",
-      ],
-    })
-    return config;
   }
-});
+}));
