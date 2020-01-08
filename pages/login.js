d3f5294ea002fdb78ab784590e085b9fa23f4127
Ty Mick <ty@tymick.me>
2020-01-08 15:09:20 -0600

    Add loading buttons to /register and /login

--- pages/login.js
+++ pages/login.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Form,
   FormGroup,
@@ -8,7 +8,8 @@ import {
   Input,
   FormFeedback,
   Button,
-  Row
+  Row,
+  Spinner
 } from "reactstrap";
 import Link from "next/link";
 import LoginLayout from "../components/login-layout";
@@ -19,117 +20,136 @@ import { login } from "../utils/auth";
 
 import "../styles.scss";
 
-export default () => (
-  <LoginLayout pageTitle="Login">
-    <Formik
-      initialValues={{ usernameOrEmail: "", password: "", remember: false }}
-      validationSchema={Yup.object({
-        usernameOrEmail: Yup.string().required(),
-        password: Yup.string().required(),
-        remember: Yup.boolean()
-      })}
-      onSubmit={async values => {
-        try {
-          const response = await fetch("/api/login", {
-            method: "POST",
-            headhers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              usernameOrEmail: values.usernameOrEmail,
-              password: values.password
-            })
-          });
-          if (response.status === 200) {
-            const { token } = await response.json();
-            login({ token }, values.remember);
-          } else {
-            console.log("Login failed.");
-            // https://github.com/developit/unfetch#caveats
-            let error = new Error(response.statusText);
-            error.response = response;
-            throw error;
+export default function Login() {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoginLayout pageTitle="Login">
+      <Formik
+        initialValues={{ usernameOrEmail: "", password: "", remember: false }}
+        validationSchema={Yup.object({
+          usernameOrEmail: Yup.string().required(),
+          password: Yup.string().required(),
+          remember: Yup.boolean()
+        })}
+        onSubmit={async values => {
+          setLoading(true);
+
+          try {
+            const response = await fetch("/api/login", {
+              method: "POST",
+              headhers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                usernameOrEmail: values.usernameOrEmail,
+                password: values.password
+              })
+            });
+            if (response.status === 200) {
+              const { token } = await response.json();
+              login({ token }, values.remember);
+            } else {
+              console.log("Login failed.");
+              // https://github.com/developit/unfetch#caveats
+              let error = new Error(response.statusText);
+              error.response = response;
+              throw error;
+            }
+          } catch (err) {
+            console.error(
+              "You have an error in your code or there are network issues.",
+              err
+            );
           }
-        } catch (err) {
-          console.error(
-            "You have an error in your code or there are network issues.",
-            err
-          );
-        }
-      }}
-    >
-      {formik => (
-        <Form
-          noValidate
-          onSubmit={formik.handleSubmit}
-          className="mx-auto"
-          style={{ width: "420px" }}
-        >
-          <FormGroup>
-            <Label for="usernameOrEmail" className="text-dark">
-              Username or email
-            </Label>
-            <Input
-              name="usernameOrEmail"
-              id="usernameOrEmail"
-              type="text"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.usernameOrEmail}
-              invalid={
-                formik.touched.usernameOrEmail &&
-                !!formik.errors.usernameOrEmail
-              }
-            />
-            <FormFeedback>{formik.errors.usernameOrEmail}</FormFeedback>
-          </FormGroup>
 
-          <FormGroup>
-            <Label for="password" className="text-dark">
-              Password
-            </Label>
-            <Input
-              name="password"
-              id="password"
-              type="password"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
-              invalid={formik.touched.password && !!formik.errors.password}
-            />
-            <FormFeedback>{formik.errors.password}</FormFeedback>
-          </FormGroup>
-
-          <FormGroup check>
-            <Label check className="text-dark">
+          setLoading(false);
+        }}
+      >
+        {formik => (
+          <Form
+            noValidate
+            onSubmit={formik.handleSubmit}
+            className="mx-auto"
+            style={{ width: "420px" }}
+          >
+            <FormGroup>
+              <Label for="usernameOrEmail" className="text-dark">
+                Username or email
+              </Label>
               <Input
-                name="remember"
-                type="checkbox"
+                name="usernameOrEmail"
+                id="usernameOrEmail"
+                type="text"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                value={formik.values.remember}
-              />{" "}
-              Remember me
-            </Label>
-          </FormGroup>
+                value={formik.values.usernameOrEmail}
+                invalid={
+                  formik.touched.usernameOrEmail &&
+                  !!formik.errors.usernameOrEmail
+                }
+              />
+              <FormFeedback>{formik.errors.usernameOrEmail}</FormFeedback>
+            </FormGroup>
 
-          <Row>
-            <div className="update ml-auto mr-auto">
-              <Button className="btn-round" color="primary" type="submit">
-                Log in
-              </Button>
-            </div>
-          </Row>
+            <FormGroup>
+              <Label for="password" className="text-dark">
+                Password
+              </Label>
+              <Input
+                name="password"
+                id="password"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
+                invalid={formik.touched.password && !!formik.errors.password}
+              />
+              <FormFeedback>{formik.errors.password}</FormFeedback>
+            </FormGroup>
 
-          <Row>
-            <div className="update ml-auto mr-auto mb-2">
-              Don't have an account yet?{" "}
-              <Link href="/register">
-                <a>Register</a>
-              </Link>
-              .
-            </div>
-          </Row>
-        </Form>
-      )}
-    </Formik>
-  </LoginLayout>
-);
+            <FormGroup check>
+              <Label check className="text-dark">
+                <Input
+                  name="remember"
+                  type="checkbox"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.remember}
+                />{" "}
+                Remember me
+              </Label>
+            </FormGroup>
+
+            <Row>
+              <div className="update ml-auto mr-auto">
+                {loading ? (
+                  <Button
+                    className="btn-round"
+                    color="primary"
+                    type="submit"
+                    disabled
+                  >
+                    <Spinner size="sm" />
+                  </Button>
+                ) : (
+                  <Button className="btn-round" color="primary" type="submit">
+                    Log in
+                  </Button>
+                )}
+              </div>
+            </Row>
+
+            <Row>
+              <div className="update ml-auto mr-auto mb-2">
+                Don't have an account yet?{" "}
+                <Link href="/register">
+                  <a>Register</a>
+                </Link>
+                .
+              </div>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+    </LoginLayout>
+  );
+}
--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Form,
   FormGroup,
@@ -11,7 +11,8 @@ import {
   Input,
   FormFeedback,
   Button,
-  Row
+  Row,
+  Spinner
 } from "reactstrap";
 import Link from "next/link";
 import LoginLayout from "../components/login-layout";
@@ -22,137 +23,159 @@ import { login } from "../utils/auth";
 
 import "../styles.scss";
 
-export default () => (
-  <LoginLayout pageTitle="Register">
-    <Formik
-      initialValues={{ username: "", password: "", retypePassword: "" }}
-      validationSchema={Yup.object({
-        username: Yup.string()
-          .matches(
-            /^\w+$/,
-            "Usernames can only consist of letters, numbers, and underscores."
-          )
-          .required("Please choose a username."),
-        password: Yup.string().required("Please choose a password."),
-        retypePassword: Yup.string().required("Just to make sure.")
-      })}
-      onSubmit={async values => {
-        if (values.password != values.retypePassword) {
-          setFieldError("retypePassword", "Oops, your passwords don't match!");
-        } else {
-          try {
-            const response = await fetch("/api/register", {
-              method: "POST",
-              headhers: { "Content-Type": "application/json" },
-              body: JSON.stringify(values)
-            });
-            if (response.status === 200) {
-              const { token } = await response.json();
-              login({ token }, false);
-            } else if (response.status === 409) {
-              setFieldError("username", "That username is already taken.");
-            } else {
-              console.log("Registration failed.");
-              // https://github.com/developit/unfetch#caveats
-              let error = new Error(response.statusText);
-              error.response = response;
-              throw error;
-            }
-          } catch (err) {
-            console.error(
-              "You have an error in your code or there are network issues.",
-              err
+export default function Register() {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <LoginLayout pageTitle="Register">
+      <Formik
+        initialValues={{ username: "", password: "", retypePassword: "" }}
+        validationSchema={Yup.object({
+          username: Yup.string()
+            .matches(
+              /^\w+$/,
+              "Usernames can only consist of letters, numbers, and underscores."
+            )
+            .required("Please choose a username."),
+          password: Yup.string().required("Please choose a password."),
+          retypePassword: Yup.string().required("Just to make sure.")
+        })}
+        onSubmit={async values => {
+          setLoading(true);
+
+          if (values.password != values.retypePassword) {
+            setFieldError(
+              "retypePassword",
+              "Oops, your passwords don't match!"
             );
+          } else {
+            try {
+              const response = await fetch("/api/register", {
+                method: "POST",
+                headhers: { "Content-Type": "application/json" },
+                body: JSON.stringify(values)
+              });
+              if (response.status === 200) {
+                const { token } = await response.json();
+                login({ token }, false);
+              } else if (response.status === 409) {
+                setFieldError("username", "That username is already taken.");
+              } else {
+                console.log("Registration failed.");
+                // https://github.com/developit/unfetch#caveats
+                let error = new Error(response.statusText);
+                error.response = response;
+                throw error;
+              }
+            } catch (err) {
+              console.error(
+                "You have an error in your code or there are network issues.",
+                err
+              );
+            }
           }
-        }
-      }}
-    >
-      {formik => (
-        <Form
-          noValidate
-          onSubmit={formik.handleSubmit}
-          className="mx-auto"
-          style={{ width: "420px" }}
-        >
-          <FormGroup>
-            <Label for="username" className="text-dark">
-              Username
-            </Label>
-            <InputGroup>
-              <InputGroupAddon addonType="prepend">
-                <InputGroupText id="ampersand">@</InputGroupText>
-              </InputGroupAddon>
+
+          setLoading(false);
+        }}
+      >
+        {formik => (
+          <Form
+            noValidate
+            onSubmit={formik.handleSubmit}
+            className="mx-auto"
+            style={{ width: "420px" }}
+          >
+            <FormGroup>
+              <Label for="username" className="text-dark">
+                Username
+              </Label>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">
+                  <InputGroupText id="ampersand">@</InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  name="username"
+                  id="username"
+                  aria-describedby="ampersand"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.username}
+                  invalid={formik.touched.username && !!formik.errors.username}
+                />
+                <FormFeedback>{formik.errors.username}</FormFeedback>
+              </InputGroup>
+            </FormGroup>
+
+            <FormGroup>
+              <Label for="password" className="text-dark">
+                Password
+              </Label>
               <Input
-                name="username"
-                id="username"
-                aria-describedby="ampersand"
-                type="text"
+                name="password"
+                id="password"
+                type="password"
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                value={formik.values.username}
-                invalid={formik.touched.username && !!formik.errors.username}
+                value={formik.values.password}
+                invalid={formik.touched.password && !!formik.errors.password}
               />
-              <FormFeedback>{formik.errors.username}</FormFeedback>
-            </InputGroup>
-          </FormGroup>
+              <FormFeedback>{formik.errors.password}</FormFeedback>
+            </FormGroup>
 
-          <FormGroup>
-            <Label for="password" className="text-dark">
-              Password
-            </Label>
-            <Input
-              name="password"
-              id="password"
-              type="password"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.password}
-              invalid={formik.touched.password && !!formik.errors.password}
-            />
-            <FormFeedback>{formik.errors.password}</FormFeedback>
-          </FormGroup>
+            <FormGroup>
+              <Label for="retypePassword" className="text-dark">
+                Retype password
+              </Label>
+              <Input
+                name="retypePassword"
+                id="retypePassword"
+                type="password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.retypePassword}
+                invalid={
+                  formik.touched.retypePassword &&
+                  formik.values.retypePassword != formik.values.password
+                }
+              />
+              <FormFeedback>
+                {formik.errors.retypePassword ||
+                  "Oops, your passwords don't match!"}
+              </FormFeedback>
+            </FormGroup>
 
-          <FormGroup>
-            <Label for="retypePassword" className="text-dark">
-              Retype password
-            </Label>
-            <Input
-              name="retypePassword"
-              id="retypePassword"
-              type="password"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.retypePassword}
-              invalid={
-                formik.touched.retypePassword &&
-                formik.values.retypePassword != formik.values.password
-              }
-            />
-            <FormFeedback>
-              {formik.errors.retypePassword ||
-                "Oops, your passwords don't match!"}
-            </FormFeedback>
-          </FormGroup>
+            <Row>
+              <div className="update ml-auto mr-auto">
+                {loading ? (
+                  <Button
+                    className="btn-round"
+                    color="primary"
+                    type="submit"
+                    disabled
+                  >
+                    <Spinner size="sm" />
+                  </Button>
+                ) : (
+                  <Button className="btn-round" color="primary" type="submit">
+                    Register
+                  </Button>
+                )}
+              </div>
+            </Row>
 
-          <Row>
-            <div className="update ml-auto mr-auto">
-              <Button className="btn-round" color="primary" type="submit">
-                Register
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <div className="update ml-auto mr-auto mb-2">
-              Already have an account?{" "}
-              <Link href="/login">
-                <a>Log in</a>
-              </Link>
-              .
-            </div>
-          </Row>
-        </Form>
-      )}
-    </Formik>
-  </LoginLayout>
-);
+            <Row>
+              <div className="update ml-auto mr-auto mb-2">
+                Already have an account?{" "}
+                <Link href="/login">
+                  <a>Log in</a>
+                </Link>
+                .
+              </div>
+            </Row>
+          </Form>
+        )}
+      </Formik>
+    </LoginLayout>
+  );
+}
