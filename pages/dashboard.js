9c735595c5a71d5785e5f868e77378c01efe9ce4
Ty Mick <ty@tymick.me>
2020-01-14 17:47:14 -0600

    Notate intentional blankness of /dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,13 +1,18 @@
 "use strict";
 
 import React from "react";
+import { Container, Card } from "reactstrap";
 import Layout from "../components/layout";
 import "../styles.scss";
 import { withAuthSync } from "../utils/auth";
 
 const Dashboard = () => (
   <Layout pageTitle="Dashboard">
-    
+    <div className="content">
+      <Container fluid>
+        <div class="h4 text-center"><em>This page intentionally left blank.</em></div>
+      </Container>
+    </div>
   </Layout>
 );
 
