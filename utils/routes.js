f79bc9ecad57a53fa2323762c741612382b8d279
Ty Mick <ty@tymick.me>
2020-01-14 17:25:53 -0600

    Remove /events, /friends, /groups

--- utils/routes.js
+++ utils/routes.js
@@ -7,21 +7,6 @@ export default [
     icon: "nc-icon nc-layout-11"
   },
   {
-    path: "/events",
-    name: "Events",
-    icon: ""
-  },
-  {
-    path: "/friends",
-    name: "Friends",
-    icon: ""
-  },
-  {
-    path: "/groups",
-    name: "Groups",
-    icon: ""
-  },
-  {
     path: "/settings",
     name: "Settings",
     icon: "nc-icon nc-settings-gear-65"
