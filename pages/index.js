7e8bc97ed507b2b8b982fa4ec9d6c7123321fc97
Ty Mick <ty@tymick.me>
2020-01-16 17:03:30 -0600

    Curl quotes

--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,8 @@ export default () => {
 
             <p>Greetings, traveler!</p>
             <p>
-              This is the beginning stages of an app I'm building. As long as
-              the app itself is half-finished, I figure this shell is a
+              This is the beginning stages of an app I&rsquo;m building. As long
+              as the app itself is half-finished, I figure this shell is a
               satisfactory portfolio piece in the meantime. Please enjoy{" "}
               <a href="https://github.com/TyWMick/nextjs-local-authentication">
                 the source code
@@ -50,8 +50,8 @@ export default () => {
               </li>
               <li>
                 The dashboard and settings pages require aunthentication, so if
-                you try to visit either before logging in, you'll be redirected
-                to the login page.
+                you try to visit either before logging in, you&rsquo;ll be
+                redirected to the login page.
               </li>
             </ul>
 
@@ -62,16 +62,16 @@ export default () => {
                 <Link href="/register">
                   <a>register</a>
                 </Link>{" "}
-                with any username (that doesn't currently exist in the database)
-                and password you like, and your credentials (after your password
-                has been hashed with{" "}
+                with any username (that doesn&rsquo;t currently exist in the
+                database) and password you like, and your credentials (after
+                your password has been hashed with{" "}
                 <a href="https://github.com/kelektiv/node.bcrypt.js">bcrypt</a>)
                 will be stored in a database managed by{" "}
                 <a href="https://www.mongodb.com/cloud/atlas">MongoDB Atlas</a>{" "}
                 on a free plan.
               </li>
               <li>
-                If you'd rather not create an account, you can instead{" "}
+                If you&rsquo;d rather not create an account, you can instead{" "}
                 <Link href="/login">
                   <a>log in</a>
                 </Link>{" "}
@@ -79,11 +79,11 @@ export default () => {
                 (I promise I never use that in real life).
               </li>
               <li>
-                Once you've logged in, the{" "}
+                Once you&rsquo;ve logged in, the{" "}
                 <Link href="/dashboard">
                   <a>dashboard page</a>
                 </Link>{" "}
-                isn't terribly interesting, but if you head on over to{" "}
+                isn&rsquo;t terribly interesting, but if you head on over to{" "}
                 <Link href="/settings">
                   <a>Settings</a>
                 </Link>
@@ -91,10 +91,10 @@ export default () => {
                 your account, which completely removes its database entry.
               </li>
               <li>
-                If you try fiddling around with multiple accounts, you'll see
-                that you can't register an account with a username that already
-                exists, and you also can't change your username to that of
-                another account.
+                If you try fiddling around with multiple accounts, you&rsquo;ll
+                see that you can&rsquo;t register an account with a username
+                that already exists, and you also can&rsquo;t change your
+                username to that of another account.
               </li>
             </ul>
 
@@ -102,18 +102,18 @@ export default () => {
             <ul>
               <li>
                 Authentication and session information are facilitated by a
-                browser cookie named "token", which you can see with your
-                browser's web development tools. If you do not check the
-                "remember me" box on the login page, this cookie will expire
-                when you end your browser session (i.e., quit the browser
-                program, not just close the tab or window). If you do check the
-                box, this cookie will expire in two weeks.
+                browser cookie named &ldquo;token&rdquo;, which you can see with
+                your browser&rsquo;s web development tools. If you do not check
+                the &ldquo;remember me&rdquo; box on the login page, this cookie
+                will expire when you end your browser session (i.e., quit the
+                browser program, not just close the tab or window). If you do
+                check the box, this cookie will expire in two weeks.
               </li>
               <li>
                 If you open the dashboard and/or settings pages in multiple
                 browser tabs, when you log out in one tab (in the profile
-                dropdown in the top-right of the page), you'll automatically be
-                logged out of all other tabs.
+                dropdown in the top-right of the page), you&rsquo;ll
+                automatically be logged out of all other tabs.
               </li>
             </ul>
 
@@ -135,8 +135,8 @@ export default () => {
               <li>
                 Design work also involved the{" "}
                 <a href="https://getbootstrap.com/">Bootstrap</a> library via{" "}
-                <a href="https://reactstrap.github.io/">reactstrap</a>. I'll
-                admit, I like{" "}
+                <a href="https://reactstrap.github.io/">reactstrap</a>.
+                I&rsquo;ll admit, I like{" "}
                 <a href="https://react-bootstrap.netlify.com/">
                   React Bootstrap
                 </a>{" "}
@@ -148,8 +148,8 @@ export default () => {
             <h2 className="h6">Serverless functions</h2>
             <ul>
               <li>
-                This demo's APIs (and server-rendered pages) are serverless
-                functions created using{" "}
+                This demo&rsquo;s APIs (and server-rendered pages) are
+                serverless functions created using{" "}
                 <a href="https://nextjs.org/">Next.js</a> and hosted on{" "}
                 <a href="https://zeit.co/">ZEIT Now</a>.
               </li>
