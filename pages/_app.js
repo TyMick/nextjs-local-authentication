efc0d4409e935f19658ee8fb9236f295f79af43d
Ty Mick <ty@tymick.me>
2020-04-02 16:49:51 -0400

    Add router progress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 "use strict";
 
+import Router from "next/router";
+import NProgress from "nprogress";
 import "../styles.scss";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
