9b8ecaf47cd3e119879abf0cd23e6bb581f86a7e
Ty Mick <ty@tymick.me>
2021-06-05 20:11:28 -0700

    Fix vulnerabilities

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 "use strict";
 
 require("dotenv").config();
-const withSass = require("@zeit/next-sass");
-const withFonts = require("next-fonts");
 
-module.exports = withSass(
-  withFonts({
-    env: {
-      DB: process.env.DB,
-      HOST: process.env.HOST
-    }
-  })
-);
+module.exports = {
+  env: {
+    DB: process.env.DB,
+    HOST: process.env.HOST,
+  },
+};
